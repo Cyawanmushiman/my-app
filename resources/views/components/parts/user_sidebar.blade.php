091804860de9799c8b8d65c1f2dde05ad980ca04
Ogawa Eiji <eogawa@minna-systems.co.jp>
2023-11-04 10:53:07 +0900

    fix: サイドバー修正

--- resources/views/components/parts/user_sidebar.blade.php
+++ resources/views/components/parts/user_sidebar.blade.php
@@ -1,6 +1,7 @@
 <div class="container-fluid px-5 my-5">
     <div class="row justify-content-center">
         <ul class="list-group list-group-flush accordion" id="accordion">
+            <a href="{{ route('user.home') }}" class="list-group-item list-group-item-action"><i class="fas fa-home me-1"></i>ホーム</a>
             <a href="{{ route('user.long_run_goals.index') }}" class="list-group-item list-group-item-action"><i class="fas fa-flag-checkered me-1"></i>長期目標の管理</a>
             <a href="{{ route('user.middle_run_goals.index') }}" class="list-group-item list-group-item-action"><i class="far fa-flag me-1"></i>中期目標の管理</a>
         </ul>
