9f63757064239fc9eed764dca38dad93dbda1292
Ogawa Eiji <eogawa@minna-systems.co.jp>
2023-11-11 16:14:59 +0900

    feat: 長期目標は1つだけに修正

--- app/Http/Requests/User/LongRunGoalController/StoreRequest.php
+++ app/Http/Requests/User/LongRunGoalController/StoreRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests\User\LongRunGoalController;
 
+use App\Models\LongRunGoal;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Validator;
 
 class StoreRequest extends FormRequest
 {
@@ -26,6 +28,16 @@ class StoreRequest extends FormRequest
         return [
             'title' => ['required', 'string', 'max:255', 'unique:long_run_goals,title,'],
         ];
+    }
+
+    // 登録できるのは1つまで
+    public function withValidator(Validator $validator): void
+    {
+        $validator->after(function ($validator) {
+            if (LongRunGoal::where('user_id', auth()->id())->count() >= 1) {
+                $validator->errors()->add('title', '登録できるのは1つまでです');
+            }
+        });
     }
 
     /**
--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -2,22 +2,33 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <x-parts.basic_card_layout>
+        <x-slot name="cardHeader">
+            <h4 class="my-2">今日の目標</h4>
+        </x-slot>
+        <x-slot name="cardBody">
+            <form method="POST" action=""  enctype="multipart/form-data">
+                @csrf
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                <div class="form-body">
+                    <div class="row">
+                        <div class="col-md-6 text-md-end">
+                            <label class="col-form-label">入社年月日@include('components.parts.required_badge')</label>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                        <div class="col-md-2 form-group">
+                            @include('components.form.date', ['name' => 'entry_on', 'required' => true])
+                            @include('components.form.error', ['name' => 'entry_on'])
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </div>
+
+                <div class="text-center my-4">
+                    <button type="submit" class="btn btn-primary" >
+                        登録する
+                    </button>
+                </div>
+            </form>
+        </x-slot>
+    </x-parts.basic_card_layout>
 </div>
 @endsection
