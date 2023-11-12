1223dad7b2d521c0905156edfc71b8c53d411bba
Ogawa Eiji <eogawa@minna-systems.co.jp>
2023-11-12 19:45:39 +0900

    feat: リファクタリング

--- app/Http/Controllers/User/InspireController.php
+++ app/Http/Controllers/User/InspireController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\User;
 use App\Models\Inspire;
 use App\Library\FileLibrary;
 use Illuminate\Http\Request;
+use App\Enums\DefaultInspire;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
 use App\Http\Requests\User\InspireController\StoreRequest;
@@ -112,43 +113,14 @@ class InspireController extends Controller
     public function setDefault(): RedirectResponse
     {
         $userId = auth()->id();
-        $bulkInsertData = [
-            [
+        $bulkInsertData = [];
+        foreach (DefaultInspire::getData() as $defaultInspire) {
+            $bulkInsertData[] = [
                 'user_id' => $userId,
-                'image_url' => '/images/inspires/cup.svg',
-                'comment' => 'コーヒーが冷めないうちに飲むのが一番だね。今のチャンスも同じ。今、君が頑張っていることは、きっと最高のタイミングなんだ。',
-            ],
-            [
-                'user_id' => $userId,
-                'image_url' => '/images/inspires/baseball.svg',
-                'comment' => 'お前が落ち込むなんて、許せない。失敗したって、それがお前の経験になるんだ。お前はこれからもっとすごいことをやってみせる。俺はお前のことを誇りに思っている。',
-            ],
-            [
-                'user_id' => $userId,
-                'image_url' => '/images/inspires/business.svg',
-                'comment' => '成功への道は、自信を持って大胆に歩むことから始まるんだ。君もその一歩を踏み出せば、きっと大きな成果が待っている。恐れずに進もう！',
-            ],
-            [
-                'user_id' => $userId,
-                'image_url' => '/images/inspires/hat.svg',
-                'comment' => '困難に立ち向かうのは勇気がいることです。あなたはその勇気を持っています。だから、諦めないでください。あなたならできます。',
-            ],
-            [
-                'user_id' => $userId,
-                'image_url' => '/images/inspires/dance.svg',
-                'comment' => 'ダンスは止まらない。常に新しいことに挑戦する。',
-            ],
-            [
-                'user_id' => $userId,
-                'image_url' => '/images/inspires/running.svg',
-                'comment' => '最後まで諦めない。走り切るのは自分のためだ。',
-            ],
-            [
-                'user_id' => $userId,
-                'image_url' => '/images/inspires/soccer.svg',
-                'comment' => 'お前たちには止められない！俺のドリブルは最強だ！さあ、シュートだ！ゴールは俺のものだ！',
-            ],
-        ];
+                'image_url' => $defaultInspire['image_url'],
+                'comment' => $defaultInspire['comment'],
+            ];
+        }
 
         Inspire::insert($bulkInsertData);
 
