90f2a694bde56c89875e6fc99ae1999296c48969
Cyawanmushiman <smallriver1878@gmail.com>
2024-04-24 21:25:42 +0900

    home修正

--- public/webhook.php
+++ public/webhook.php
@@ -8,7 +8,8 @@ $hmac = hash_hmac('sha1', $HTTP_RAW_POST_DATA, $SECRET_KEY);
 if ( isset($header['X-Hub-Signature']) && $header['X-Hub-Signature'] === 'sha1='.$hmac ) {
     $payload = json_decode($HTTP_RAW_POST_DATA, true);  // 受け取ったJSONデータ
     // ここに実行したいコードを書く
-    exec('git pull origin main 2>&1');
+    // exec('git pull origin main 2>&1');
+    exec('cd /home/bitnami/laravel-project/my-app && git pull origin main');
     file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." ".$_SERVER['REMOTE_ADDR']." git pulled: ".$payload['after']." ".$payload['commits'][0]['message']."\n", FILE_APPEND|LOCK_EX);
 } else {
     // 認証失敗
