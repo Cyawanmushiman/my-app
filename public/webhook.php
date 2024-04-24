3296e0a7e23ff81db7acc0339b841216f59a9fcd
Cyawanmushiman <smallriver1878@gmail.com>
2024-04-24 21:37:50 +0900

    ログファイルの箇所を修正

--- public/webhook.php
+++ public/webhook.php
@@ -1,6 +1,5 @@
 <?php
-// 設定
-$LOG_FILE = dirname(__FILE__).'/webhook.log';
+$LOG_FILE = '/home/bitnami/laravel-project/my-app/storage/logs/webhook.log';
 $SECRET_KEY = 'Cyawanmushi0314';
 
 $header = getallheaders();
@@ -10,7 +9,10 @@ if ( isset($header['X-Hub-Signature']) && $header['X-Hub-Signature'] === 'sha1='
     // ここに実行したいコードを書く
     exec('git pull origin main 2>&1');
     // exec('cd /home/bitnami/laravel-project/my-app && git pull origin main');
-    file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." ".$_SERVER['REMOTE_ADDR']." git pulled: ".$payload['after']." ".$payload['commits'][0]['message']."\n", FILE_APPEND|LOCK_EX);
+    
+    // ログを残す
+    file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." git pull\n", FILE_APPEND|LOCK_EX);
+    
 } else {
     // 認証失敗
     file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." invalid access: ".$_SERVER['REMOTE_ADDR']."\n", FILE_APPEND|LOCK_EX);
