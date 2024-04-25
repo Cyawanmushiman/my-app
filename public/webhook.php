79bd5fef79d627f71030fc35a297ce1500435672
Cyawanmushiman <smallriver1878@gmail.com>
2024-04-25 22:54:21 +0900

    微修正

--- public/webhook.php
+++ public/webhook.php
@@ -3,6 +3,7 @@ $LOG_FILE = dirname(__FILE__) . '/webhook.log';
 
 // $SECRET_KEY = 'Cyawanmushi0314';
 file_put_contents( $LOG_FILE, date( "[Y-m-d H:i:s]" ) . " Start Deploy\n", FILE_APPEND | LOCK_EX );
+// コメント追加
 
 // $header = getallheaders();
 // $hmac = hash_hmac('sha1', $HTTP_RAW_POST_DATA, $SECRET_KEY);
