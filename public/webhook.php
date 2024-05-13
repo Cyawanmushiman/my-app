56275644c7de71c666a698db308951b4ec8331d8
Cyawanmushiman <smallriver1878@gmail.com>
2024-05-13 20:15:42 +0900

    fix:微修正

--- public/webhook.php
+++ public/webhook.php
@@ -1,26 +1,26 @@
 <?php
 // エラーログの有効化
-ini_set('display_errors', 1);
-ini_set('log_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('log_errors', 1);
+// error_reporting(E_ALL);
 
 // POSTデータの取得
-$postData = file_get_contents('php://input');
+// $postData = file_get_contents('php://input');
 
 // データのロギング（デバッグ用）
-file_put_contents('webhook.log', $postData . "\n", FILE_APPEND);
+// file_put_contents('webhook.log', $postData . "\n", FILE_APPEND);
 // \Log::debug($postData);
 
 // whoamiコマンドの実行結果を取得
-exec('whoami', $whoamiOutput);
-file_put_contents('webhook.log', 'whoami result: ' . implode("\n", $whoamiOutput) . "\n", FILE_APPEND);
+// exec('whoami', $whoamiOutput);
+// file_put_contents('webhook.log', 'whoami result: ' . implode("\n", $whoamiOutput) . "\n", FILE_APPEND);
 
-$output = null;
-exec('cd /home/bitnami/laravel-project/my-app && git pull origin main 2>&1', $output);
-file_put_contents('webhook.log', 'execコマンドの結果: ' . implode("\n", $output) . "\n", FILE_APPEND);
+// $output = null;
+// exec('cd /home/bitnami/laravel-project/my-app && git pull origin main 2>&1', $output);
+// file_put_contents('webhook.log', 'execコマンドの結果: ' . implode("\n", $output) . "\n", FILE_APPEND);
 
 // 何かしらのレスポンスを返す
-http_response_code(200);
+// http_response_code(200);
 
 // $LOG_FILE = dirname(__FILE__) . '/webhook.log';
 
