808f4278ddff26301e5520be006d30023472830c
Cyawanmushiman <smallriver1878@gmail.com>
2024-06-02 21:23:14 +0900

    編集画面のリファクタリング

---
+++ resources/views/components/parts/mind_maps/lupe_buttons.blade.php
@@ -0,0 +1,4 @@
+{{-- マインドマップ拡大 --}}
+<button type="button" class="btn  btn-sm btn-outline-secondary" id="zoomIn"><i class="fa-solid fa-magnifying-glass-plus"></i></button>
+{{-- マインドマップ縮小 --}}
+<button type="button" class="btn  btn-sm btn-outline-secondary" id="zoomOut"><i class="fa-solid fa-magnifying-glass-minus"></i></button>
---
+++ resources/views/components/parts/mind_maps/node_color_change_buttons.blade.php
@@ -0,0 +1,8 @@
+{{-- カラー変更→default --}}
+<button type="button" class="btn  btn-sm btn-outline-secondary" id="chage_air"><i class="fa-solid fa-droplet"></i></button>
+{{-- カラー変更→gray --}}
+<button type="button" class="btn  btn-sm btn-secondary text-white" id="chage_gray"><i class="fa-solid fa-droplet"></i></button>
+{{-- カラー変更→orange --}}
+<button type="button" class="btn  btn-sm btn-primary text-white" id="change_orange"><i class="fa-solid fa-droplet"></i></button>
+{{-- カラー変更→blue --}}
+<button type="button" class="btn  btn-sm btn-info text-white" id="change_blue"><i class="fa-solid fa-droplet"></i></button>
