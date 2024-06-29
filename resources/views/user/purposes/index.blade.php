633a4391e100b69c5b6ab4d06fed046fa14d9cb1
Cyawanmushiman <smallriver1878@gmail.com>
2024-06-29 18:05:59 +0900

    ホバーした際の挙動を修正

--- resources/views/user/purposes/index.blade.php
+++ resources/views/user/purposes/index.blade.php
@@ -7,28 +7,12 @@
             <div class="d-flex mb-5 align-items-center">
                 <div class="col-11 position-relative">
                     <img src="{{ asset('images/gifs/cat-8915_128.gif') }}" alt="" style="width: 30px; height: 30px; top: -14px; left: 25%" class="position-absolute translate-middle">
-                    {{-- <div style=
-                    "width: 15px; 
-                    height: 15px; 
-                    background-color: #fff;
-                    border: 2px solid #bd5d38;
-                    border-radius: 50%;
-                    top: 2px;
-                    left: 105%;
-                    " class="position-absolute translate-middle"
-                    ></div> --}}
                     <div class="progress" role="progressbar" aria-label="Example 20px high" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style="height: 5px">
                         <div class="progress-bar bg-info" style="width: 25%"></div>
                     </div>
                 </div>
                 <div class="col-1 d-flex justify-content-end">
-                    <div style=
-                    "width: 15px; 
-                    height: 15px; 
-                    background-color: #fff;
-                    border: 2px solid #bd5d38;
-                    border-radius: 50%;
-                    " data-bs-toggle="tooltip" data-bs-placement="top" title={{ $purpose->content }}>
+                    <div class="purpose-tooltip" data-bs-toggle="tooltip" data-bs-placement="top" title={{ $purpose->content }}>
                     </div>
                 </div>
             </div>
